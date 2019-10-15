f38f782ad9453f3d9a5d03f860611f2ddd957163
Phinehas <kohler.nai@gmail.com>
2019-10-14 20:12:44 -0600

    Actual landing page now has playable chessboard on it

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import './App.css';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import LandingPage from "./LandingPage"
 import ActualLandingPage from './ActualLandingPage'
+
 
 function App() {
   return (

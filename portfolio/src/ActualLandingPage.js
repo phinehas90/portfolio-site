121bfa83f20b085189011d4b80b25232d54a24aa
Phinehas <kohler.nai@gmail.com>
2019-10-16 21:03:33 -0600

    added navbar

--- portfolio/src/ActualLandingPage.js
+++ portfolio/src/ActualLandingPage.js
@@ -2,11 +2,19 @@ import React from 'react'
 import Chess from './ChessBoard'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
+import NavBar from 'react-bootstrap/Navbar'
+import NavItem from 'react-bootstrap/NavItem'
+import NavLink from 'react-bootstrap/NavLink'
 
 export default function ActualLandingPage(){
     return(
 
         <Container className='p-3'>
+            <NavBar color='light' light expand='md'>
+                <NavItem>
+                    <NavLink>Home</NavLink>
+                </NavItem>
+            </NavBar>
             <Jumbotron>Welcome to my portfolio!</Jumbotron>
             <Chess/>
         </Container>

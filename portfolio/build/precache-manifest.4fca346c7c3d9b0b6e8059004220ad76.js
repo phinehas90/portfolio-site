529a8e078bca99d88347d6a6a5342dae93ef53da
Phinehas <kohler.nai@gmail.com>
2019-10-16 21:15:16 -0600

    commented out build to see if will deploy

---
+++ portfolio/build/precache-manifest.4fca346c7c3d9b0b6e8059004220ad76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf337b0662dac29a2c2f780f09208ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "505ab05d8f0eb8028967",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2aeb705cc9952ea912eb",
+    "url": "/static/js/2.a16e4dc3.chunk.js"
+  },
+  {
+    "revision": "505ab05d8f0eb8028967",
+    "url": "/static/js/main.dc605f55.chunk.js"
+  },
+  {
+    "revision": "bfd1521e694d93483f19",
+    "url": "/static/js/runtime-main.14a2f347.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
